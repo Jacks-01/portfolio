c16068af0ade45a880ac4f8bc86afd0a9e0e5a71
Jack Stubblefield <jack011601@gmail.com>
2023-11-22 10:25:51 -0600

    nav links styling and added a container for name box

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react';
 import './App.css';
+import Nav from './Components/Nav/Nav';
 
 interface Props {}
 
 const App: FC<Props> = ({}) => {
 	return (
 		<>
-			<p className='name'>
-				<span className='codeSymbols'>&lt;/</span>
-				Jack <br /> Stubblefield
-				<span className='codeSymbols'>/&gt;</span>
-			</p>
+			<div className='nameContainer'>
+				<p className='nameText'>
+					<span className='codeSymbols'>&lt;/</span>
+					Jack <br /> Stubblefield
+					<span className='codeSymbols'>/&gt;</span>
+				</p>
+			</div>
+			<Nav />
 		</>
 	);
 };
---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import './Nav.css';
+
+interface Props {}
+
+const Nav: FC<Props> = ({}) => {
+	return (
+		<>
+			<div className='navContainer'>
+				<a className='navLink'>
+					<p>
+						<span>/</span>about
+					</p>
+				</a>
+				<a className='navLink'>
+					<p>
+						<span>/</span>experience
+					</p>
+				</a>
+				<a className='navLink'>
+					<p>
+						<span>/</span>resume
+					</p>
+				</a>
+			</div>
+		</>
+	);
+};
+
+export default Nav;
