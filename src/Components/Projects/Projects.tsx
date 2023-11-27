f6bc948d0366133afaea44f21fc6cb6806713567
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-11-27 16:14:01 -0600

    changed the layered waves to a singular wave to make the design consistent

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -6,12 +6,29 @@ interface Props {}
 const Projects: FC<Props> = ({}) => {
 	return (
 		<>
-			<div className='spacer squiggle'></div>
-			<div className='projectContainer'>
-				<p id='project' className='project'>Projects</p>
-				<div className='projectParagraph'>
-					<section className='job'>
-						<h3>Example Project</h3>
+			<div className='spacerBlue squiggle'></div>
+			<div className='projectsContainer'>
+				<p id='projects' className='projects'>Projects</p>
+				<div className='projectsParagraph'>
+					<section className='project'>
+						<h3>
+							Example Project
+						</h3>
+						<p>
+							Gochujang post-ironic fit tousled echo park
+							microdosing. Paleo pinterest JOMO, blog put a bird
+							on it XOXO cray mukbang quinoa. Ethical you probably
+							haven't heard of them cold-pressed mumblecore,
+							helvetica tousled iPhone yr banjo neutra
+							single-origin coffee. Fingerstache jianbing
+							vexillologist pour-over, irony fanny pack JOMO twee.
+							Biodiesel big mood tilde, fanny pack neutra kombucha
+							ennui whatever affogato.
+						</p>
+					</section>	
+
+					<section className='project'>
+						<h3>Another Example Project</h3>
 						<p>
 							8-bit wayfarers jianbing subway tile, health goth
 							snackwave post-ironic stumptown skateboard banjo
