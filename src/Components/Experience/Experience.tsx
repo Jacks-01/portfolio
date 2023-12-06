8399a9882be758031f059049f723e23fd3cfd645
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-06 10:25:13 -0600

    finished experience section

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -8,7 +8,11 @@ const Experience: FC<Props> = ({}) => {
 		<>
 			<div className='spacer curve'></div>
 			<div className='experienceContainer'>
-				<p id='experience' className='experience'>Experience</p>
+				<p
+					id='experience'
+					className='experience'>
+					Experience
+				</p>
 				<div className='experienceParagraph'>
 					<section className='job'>
 						<h3>
@@ -17,26 +21,13 @@ const Experience: FC<Props> = ({}) => {
 						</h3>
 						<h4>April 2023 - Current</h4>
 						<p>
-							8-bit wayfarers jianbing subway tile, health goth
-							snackwave post-ironic stumptown skateboard banjo
-							bushwick neutra drinking vinegar shaman leggings.
-							Deep v lumbersexual bitters godard vaporware
-							wayfarers gatekeep tote bag butcher. Marfa health
-							goth gatekeep cardigan schlitz kitsch taxidermy
-							bespoke Brooklyn chicharrones yr kale chips vegan
-							semiotics. Raclette jean shorts dreamcatcher
-							fingerstache coloring book.
-						</p>
-						<p>
-							Gochujang post-ironic fit tousled echo park
-							microdosing. Paleo pinterest JOMO, blog put a bird
-							on it XOXO cray mukbang quinoa. Ethical you probably
-							haven't heard of them cold-pressed mumblecore,
-							helvetica tousled iPhone yr banjo neutra
-							single-origin coffee. Fingerstache jianbing
-							vexillologist pour-over, irony fanny pack JOMO twee.
-							Biodiesel big mood tilde, fanny pack neutra kombucha
-							ennui whatever affogato.
+							During my time at RTS, I was responsible for a very
+							wide variety of technologies such as SQL Server,
+							.NET applications, SSRS, and other data solutions. I
+							also created and maintained small applications for
+							internal and external use. Examples are custom
+							dashboards, integrations between CRM's, and
+							independent applications for specific business needs
 						</p>
 					</section>
 					<section className='job'>
@@ -46,26 +37,11 @@ const Experience: FC<Props> = ({}) => {
 						</h3>
 						<h4>Feburary 2023 - May 2023</h4>
 						<p>
-							8-bit wayfarers jianbing subway tile, health goth
-							snackwave post-ironic stumptown skateboard banjo
-							bushwick neutra drinking vinegar shaman leggings.
-							Deep v lumbersexual bitters godard vaporware
-							wayfarers gatekeep tote bag butcher. Marfa health
-							goth gatekeep cardigan schlitz kitsch taxidermy
-							bespoke Brooklyn chicharrones yr kale chips vegan
-							semiotics. Raclette jean shorts dreamcatcher
-							fingerstache coloring book.
-						</p>
-						<p>
-							Gochujang post-ironic fit tousled echo park
-							microdosing. Paleo pinterest JOMO, blog put a bird
-							on it XOXO cray mukbang quinoa. Ethical you probably
-							haven't heard of them cold-pressed mumblecore,
-							helvetica tousled iPhone yr banjo neutra
-							single-origin coffee. Fingerstache jianbing
-							vexillologist pour-over, irony fanny pack JOMO twee.
-							Biodiesel big mood tilde, fanny pack neutra kombucha
-							ennui whatever affogato.
+							As an intern at Letter B, my primary task was to
+							help develop a custom CRM for internal using the
+							.NET stack. I worked with the other interns and
+							engineers to develop the CRM using best practices
+							such as TDD, Agile, and Azure CI/CD pipelines.
 						</p>
 					</section>
 
@@ -73,26 +49,12 @@ const Experience: FC<Props> = ({}) => {
 						<h3>Teaching Assistant - DeltaV Code School</h3>
 						<h4>October 2022 - May 2023</h4>
 						<p>
-							8-bit wayfarers jianbing subway tile, health goth
-							snackwave post-ironic stumptown skateboard banjo
-							bushwick neutra drinking vinegar shaman leggings.
-							Deep v lumbersexual bitters godard vaporware
-							wayfarers gatekeep tote bag butcher. Marfa health
-							goth gatekeep cardigan schlitz kitsch taxidermy
-							bespoke Brooklyn chicharrones yr kale chips vegan
-							semiotics. Raclette jean shorts dreamcatcher
-							fingerstache coloring book.
-						</p>
-						<p>
-							Gochujang post-ironic fit tousled echo park
-							microdosing. Paleo pinterest JOMO, blog put a bird
-							on it XOXO cray mukbang quinoa. Ethical you probably
-							haven't heard of them cold-pressed mumblecore,
-							helvetica tousled iPhone yr banjo neutra
-							single-origin coffee. Fingerstache jianbing
-							vexillologist pour-over, irony fanny pack JOMO twee.
-							Biodiesel big mood tilde, fanny pack neutra kombucha
-							ennui whatever affogato.
+							At DeltaV, I helped students going through the full
+							stack JavaScript development course with
+							assignments, projects, and tutoring when nessecary.
+							I also taught the introductory web development
+							classes and helped students decide if a career in
+							software development was for them.
 						</p>
 					</section>
 				</div>
