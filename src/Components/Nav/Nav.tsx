79593dd376b29d68555112c5e5d5aff94437b992
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-06 14:35:05 -0600

    added resume link

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -7,22 +7,31 @@ const Nav: FC<Props> = ({}) => {
 	return (
 		<>
 			<div className='navContainer'>
-				<a className='navLink' href='#about'>
+				<a
+					className='navLink'
+					href='#about'>
 					<p>
 						<span>/</span>about
 					</p>
 				</a>
-				<a className='navLink' href='#experience'>
+				<a
+					className='navLink'
+					href='#experience'>
 					<p>
 						<span>/</span>experience
 					</p>
 				</a>
-				<a className='navLink' href='#projects'>
+				<a
+					className='navLink'
+					href='#projects'>
 					<p>
 						<span>/</span>projects
 					</p>
 				</a>
-				<a className='navLink' href='#resume'>
+				<a
+					className='navLink'
+					href='https://docs.google.com/document/d/13KParEI4purIH_Ja6VvHBP2CUxav1IOesLrPGp09n4U/edit?usp=sharing'
+					target='_blank'>
 					<p>
 						<span>/</span>resume
 					</p>
